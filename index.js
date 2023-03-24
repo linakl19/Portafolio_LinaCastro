7d40ade5a1a8840e2e88c61d8a75c5cbe99499a1
Lina Castro <112339197+linakl19@users.noreply.github.com>
2023-03-24 11:37:05 -0500

    Anexando efectos scroll

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+//Hamburguer Menu
 const navToggle = document.querySelector(".header__toggle")
 const navMenu = document.querySelector(".menu")
-
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("menu__invisible");
@@ -13,3 +13,14 @@ navToggle.addEventListener("click", () => {
     }
 });
 
+// Efectos Scroll
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 300
+});
+ScrollReveal().reveal('.scroll__bottom', {origin: 'top'});
+ScrollReveal().reveal('.scroll__top', {origin: 'bottom'});
+ScrollReveal().reveal('.scroll__contenido', {delay: 500, origin: 'right'});
+ScrollReveal().reveal('.scroll__titulo', {delat: 400, origin: 'left'});
